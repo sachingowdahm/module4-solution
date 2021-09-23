9bbdafd650abd80ee2c5c7d4a3a95e79132352cf
sachingowdahm <sachin.g.h@campusuvce.in>
2021-09-23 16:21:03 +0530

    module-4 assignment

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+  (function () {
+    var names = ["Sachin", "John", "Cena", "Jason", "Ganesh", "Mohan", "Lohit", "Padala", "Rama", "Shyam"];
+    for (var i = 0; i < names.length; i++) {
+      var firstLetter = names[i].charAt(0).toLowerCase();
+      if (firstLetter === 'j') {
+        byeSpeaker.speak(names[i]);
+      } else {
+        helloSpeaker.speak(names[i]);
+      }
+    }
+  })();
